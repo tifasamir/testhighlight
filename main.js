b57b8712beae18761fee9e8f9895469eb0648d0c
tifasamir <mostafa87eltayeb@gmail.com>
2024-01-16 23:38:36 +0200

    test highlight

---
+++ main.js
@@ -0,0 +1,230 @@
+class Line{
+    content;
+    words ;
+    coloredContent;
+    coloredWords;
+}
+
+var txt1 = `
+1.	This is an item mainly one
+1.1.	Options to do again
+this is an option to do it agadasin ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again.
+1.2.	Second Option
+Test again man Test agasain mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again m.
+
+1.3.	Third one isso
+this is an option to dso it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again
+
+
+2.	This is an item mssainly one
+2.1.	Optssions to do again
+this is an option to do it again ang again thi
+s is an option to do it again ang again this is
+ an option to do it again ang again this is an option to d
+ o it again ang again this is an option to do it again ang a
+ gain this is an option to do it again ang again this is an 
+ option to do it again ang again this is an option to do it again
+  ang again this is an option to do it again ang again this is an option
+   to do it again ang again this is an option to do it 
+   again ang again this is an option to do it again ang again.
+2.2.	Second Option
+Test again man Test again mTest again mTest again 
+mTest again mTest again mTest again mTest again mTest again m
+Test again mTest again mTest again mTest a
+gain mTest again mTest again mTest again m.
+2.3.	Second Option
+Test again man Test again mTest again mTest agai
+n mTest again mTest again mTest again mTest again mTest again mTest again mT
+est again mTest again mTest again mTest again mTest again mTest again m.
+
+1.	This is an item mainly one
+1.1.	Options to do again
+this is an option to do it again ang 
+again this is an option to do it again ang again this is an option to do it aga
+in ang again this is an option to do it again ang again this is an option to do it again a
+ng again this is an option to do it again ang again this is an optio
+n to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again.
+1.2.	Second Option
+Test again man Test again mTest again mTest again mTest
+ again mTest again mTest again mTest again mTest again mTest aga
+ in mTest again mTest again mTest again mTest again mTest again mTest again m.
+
+1.3.	Third one isso
+this is an option to do it again ang again this is an 
+option to do it again ang again this is an option to do it 
+again ang agassin this is an option to do it again ang again thi
+s is an option to do it again ang again
+
+
+2.	This is an item mainly one
+2.1.	Options to do again
+this is an option to do it again ang again this is an optio
+n to do it again ang again this is an option to do it again ang again this is
+ an option to do it again ang again this is an option to do it again ang again 
+ this is an option to do it againss ang again this is an option to do it again ang agai
+ n this is an option to do it again ang again this is an option to do it again ang again
+  this is an option to do it again ang again this is an
+   option to do it again ang again this is an optioffn to do 
+   it again ang again.
+
+2.2.	Second Option
+Test again man Test again mTest again mTest again mTest ag
+ain mTest again mTest again mTest again mTest again mTest again mTest aga
+in mTest again mTest again mTest again mTest aasagain mTest again m.
+2.3.	Second Option
+Test again man Test again mTest again mTest again mTest a
+gain mTest again mTest again mTest again mTest aga
+
+in mTest again mTest again mTest again mTest again mTest ag
+ain mTest again mTest again m.
+`
+
+
+var txt2 = `
+1.	This is an item mainly one
+1.1.	Options to do again
+this is an option to do it agadasin ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again.
+1.2.	Second Option
+Test again man Test agasain mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again mTest again m.
+
+1.3.	Third one isso
+this is an option to dso it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again
+
+
+2.	This is an item mssainly one
+2.1.	Optssions to do again
+this is an option to do it again ang again thi
+s is an option to do it again ang again this is
+ an option to do it again ang again this is an option to d
+ o it again ang again this is an option to do it again ang a
+ gain this is an option to do it again ang again this is an 
+ option to do it again ang again this is an option to do it again
+  ang again this is an option to do it again ang again this is an option
+   to do it again ang again this is an option to do it 
+   again ang again this is an option to do it again ang again.
+2.2.	Second Option
+Test again man Test again mTest again mTest again 
+mTest again mTest again mTest again mTest again mTest again m
+Test again mTest again mTest again mTest a
+gain mTest again mTest again mTest again m.
+2.3.	Second Option
+Test again man Test again mTest again mTest agai
+n mTest again mTest again mTest again mTest again mTest again mTest again mT
+est again mTest again mTest again mTest again mTest again mTest again m.
+
+1.	This is an item mainly one
+1.1.	Options to do again
+this is an option to do it again ang 
+again this is an option to do it again ang again this is an option to do it aga
+in ang again this is an option to do it again ang again this is an option to do it again a
+ng again this is an option to do it again ang again this is an optio
+n to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again this is an option to do it again ang again.
+1.2.	Second Option
+Test again man Test again mTest again mTest again mTest
+ again mTest again mTest again mTest again mTest again mTest aga
+ in mTest again mTest again mTest again mTest again mTest again mTest again m.
+
+1.3.	Third one isso
+this is an option to do it again ang again this is an 
+option to do it again ang again this is an option to do it 
+again ang again this is an option to do it again ang again thi
+s is an option to do it again ang again
+
+
+2.	This is an item mainly one
+2.1.	Options to do again
+this is an option to do it again ang again this is an optio
+n to do it again ang again this is an option to do it again ang again this is
+ an option to do it again ang again this is an option to do it again ang again 
+ this is an option to do it again ang again this is an option to do it again ang agai
+ n this is an option to do it again ang again this is an option to do it again ang again
+  this is an option to do it again ang again this is an
+   option to do it again ang again this is an option to do 
+   it again ang again.
+
+2.2.	Second Option
+Test again man Test again mTest again mTest again mTest ag
+ain mTest again mTest again mTest again mTest again mTest again mTest aga
+in mTest again mTest again mTest again mTest again mTest again m.
+2.3.	Second Option
+Test again man Test again mTest again mTest again mTest a
+gain mTest again mTest again mTest again mTest aga
+
+in mTest again mTest again mTest again mTest again mTest ag
+ain mTest again mTest again m.
+`
+
+function processTxt(txt1,txt2){
+    var cssHighlight ='style="background-color:yellow"'
+
+   var lines1 = txt1.split("\n");
+   var lines2 = txt2.split("\n");
+
+   var editedlines1 = [] ;
+   var editedlines2 = [];
+
+
+
+   var correctedLineTxt =""
+ 
+
+   lines1.forEach(line => {
+    var words =line.split(" ")
+    var m1 =new Line()
+    m1.content =line
+    m1.words = words
+    editedlines1.push(m1)
+   })
+
+   lines2.forEach(line => {
+    var words =line.split(" ")
+    var m2=new Line()
+    m2.content =line
+    m2.words = words
+    editedlines2.push(m2)
+   })
+
+   var indexLine = 0
+   editedlines1.forEach(x1_line => {
+    // if(x1_line===editedlines2[indexLine])  
+      correctedLineTxt += `<p >`
+    // else correctedLineTxt = `<p  ${cssHighlight} >`
+ 
+    var indexWord = 0
+    
+    x1_line.words.forEach(w => {
+        if(w ===editedlines2[indexLine].words[indexWord])  
+        correctedLineTxt +=`<span>${w+' '}</span>`
+        else  correctedLineTxt +=`<span ${cssHighlight}>${w}</span>`
+        indexWord++
+    });   
+    correctedLineTxt += "</p>"
+    // editedLines.push(correctedLineTxt)
+    indexLine++
+})
+
+var correctedLineTxt2 =""
+lines1.forEach(line => {
+    correctedLineTxt2 += `<p >${line}</p>`
+   })
+document.getElementById("div1").innerHTML = correctedLineTxt
+
+document.getElementById("div2").innerHTML = correctedLineTxt2
+//     words.forEach(word => {
+
+//     })
+//     correctedLineTxt = `<p  ${cssHighlight}>`
+//     words.forEach(word => {
+
+
+//         correctedLineTxt +=`<span ${cssHighlight}>${word}</span>`
+//     });
+//     correctedLineTxt += "</p>"
+//     editedLines.push(correctedLineTxt)
+
+//     index++
+//    });
+}
+
+processTxt(txt1,txt2)
+
